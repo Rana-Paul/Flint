1d2ba17d45fe50a32b758e1d97b78667d3d38e61
Rana-Mapscrew <ranapaul741223@gmail.com>
2024-01-24 22:46:26 +0530

    carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 "use client";
+import CarouselPlayer from "@/components/Carousel/CarouselPlayer";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Scroller from "@/components/Scroller/Scroller";
 import Testimonial from "@/components/Testimonial/Testimonial";
 import Video from "@/components/Video";
 import { buttonVariants } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { ArrowRight, Loader2 } from "lucide-react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
@@ -14,7 +22,7 @@ export default function Home() {
   useEffect(() => {
     setIsClient(true);
   }, []);
-  
+
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-20 sm:mt-40 flex flex-col items-center justify-center text-center">
@@ -113,10 +121,50 @@ export default function Home() {
           </div>
         </div>
 
-        {/* steps */}
+        {/* Testimonail */}
         <Testimonial />
 
-        <div className="mx-auto max-w-6xl px-6 lg:px-8">
+        {/* Carousel */}
+
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <CarouselPlayer />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+
+        {/* <div className="mx-auto max-w-6xl px-6 lg:px-8">
           <div className="mt-16 flow-root sm:mt-12">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <Image
@@ -129,7 +177,7 @@ export default function Home() {
               />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
---
+++ src/components/Carousel/CarouselPlayer.tsx
@@ -0,0 +1,68 @@
+"use client";
+import Image from "next/image";
+import {
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  Carousel,
+} from "../ui/carousel";
+import data from "./data.json";
+
+
+function CarouselPlayer() {
+  return (
+    <div className="mx-auto max-w-5xl px-6 lg:px-8 mt-1">
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {data.data.map((item, index) => (
+            <CarouselItem key={index}>
+              <div className="grid sm:grid-cols-2 sm:gap-10">
+                <div className="">
+                  <Image
+                    src={item.img}
+                    alt="hiii"
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="w-[100%] h-[98%] rounded-lg shadow-lg "
+                  />
+                </div>
+                <div className="p-2 relative w-full sm:text-start text-center sm:w-[80%]">
+                  {/* map */}
+                  <div>
+                    <h3 className="mt-2 sm:mt-4 text-xl sm:text-2xl text-black-600 font-bold">
+                      {item.title}
+                    </h3>
+                  </div>
+
+                  <div className="mt-2 sm:mt-4 text-start w-full sm:p-0 ">
+                    {item.steps.map((item, index) => (
+                      <ul
+                        key={index}
+                        className="list-disc text-xs sm:text-sm list-outside"
+                      >
+                        <li className="sm:mt-4 text-gray-600 text-start">
+                          {item.step}
+                        </li>
+                      </ul>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              {/* <img src="public/img/step1" alt="heheh" /> */}
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
+
+export default CarouselPlayer;
